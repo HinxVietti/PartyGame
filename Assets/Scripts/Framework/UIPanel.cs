bf7b303ee69de17b146527fb40f38c373839d95d
Hinx Vietti <hinxvietti@gmail.com>
2020-01-11 15:37:43 +0800

    init commit

---
+++ Assets/Scripts/Framework/UIPanel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class UIPanel : UIBehaviour
+{
+    public virtual void Show() { }
+    public virtual void Hide() { }
+
+}
---
+++ Assets/Scripts/Menu/GameUIControl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameUIControl : UIPanel
+{
+    [SerializeField] Button StartGame = null;
+    [SerializeField] Button Setting = null;
+    [SerializeField] Button Quit = null;
+
+
+    protected override void Start()
+    {
+        RegisterEvent();
+    }
+
+    private void RegisterEvent()
+    {
+        StartGame.onClick.AddListener(StartGameClicked);
+        Setting.onClick.AddListener(GlobalSettingClick);
+        Quit.onClick.AddListener(QuitGameClick);
+    }
+
+    private void QuitGameClick()
+    {
+        Application.Quit();
+    }
+
+    private void GlobalSettingClick()
+    {
+
+    }
+
+    private void StartGameClicked()
+    {
+
+    }
+}
